29fa0c26f92313388d45d6389a875842eab61ffa
adeshinalateef <adeshinalateef@gmail.com>
2025-09-04 11:38:34 +0100

    Save local changes before rebase

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./assets/css/index.css";
+import Experience from "./pages/Experience/Experience";
+import Contact from "./pages/Contact/Contact";
+import Projects from "./pages/Projects/Projects";
+import Header from "./pages/Header/Header";
+import Hero from "./pages/Hero/Hero";
+import Skills from "./pages/Skills/Skills";
+import Education from "./pages/Education/Education";
+import Certification from "./pages/Certification/Certification";
+
+import { Route, Routes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+
+export default function App() {
+  const [isOnePage, setIsOnePage] = useState(false); // Toggle state
+
+  return (
+    <>
+      <Header />
+      {/* Conditional Rendering */}
+      {isOnePage ? (
+        // One-Page Mode: Render all components together
+        <>
+          <Hero />
+          <Skills />
+          <Experience />
+          <Education />
+          <Contact />
+        </>
+      ) : (
+        // Router Mode: Use routes for navigation
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/education" element={<Education />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/certification" element={<Certification />} />
+          {/* Default route: redirect to home */}
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      )}
+    </>
+  );
+}
